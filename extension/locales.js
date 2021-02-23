839cdaec31dfb820325b9f1772523d3d5cd7a33f
ManuTheCoder <77016441+ManuTheCoder@users.noreply.github.com>
2021-02-22 17:50:08 -0800

    Create locales.js

---
+++ extension/locales.js
@@ -0,0 +1,10 @@
+const kLocales = {
+'https://smartlist.ga/dashboard/rooms/add.php': 'Add to Kitchen',
+'https://smartlist.ga/dashboard/rooms/bathroom/add.html': 'Add to Bathroom',
+'https://smartlist.ga/dashboard/rooms/bedroom/addx.php': 'Add to Bedroom',
+'https://smartlist.ga/dashboard/rooms/family/add.html': 'Add to Family (Living) room',
+'https://smartlist.ga/dashboard/rooms/storage/add.html': 'Add to Storage room',
+'https://smartlist.ga/dashboard/rooms/dining_room/add.html': 'Add to Dining Room',
+'https://smartlist.ga/dashboard/rooms/camping/add.html': 'Add to Camping Supplies',
+'https://smartlist.ga/dashboard/rooms/laundry/add.html': 'Add to Laundry room',
+};

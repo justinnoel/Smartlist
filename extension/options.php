14462eb403f91e001e17e421b6de521ec98c3ac0
ManuTheCoder <77016441+ManuTheCoder@users.noreply.github.com>
2021-02-22 17:51:39 -0800

    Create options.php

---
+++ extension/options.php
@@ -0,0 +1,160 @@
+<html>
+
+<title>New Tab</title>
+<script src="//instant.page/5.1.0" type="module" integrity="sha384-by67kQnR+pyfy8yWP4kPO12fHKRLHZPfEsiSXR8u2IKcTdxD805MGUXBzVPnkLHw"></script>
+    <link rel="prefetch" href="gadget.html" />
+        <link rel="preload" href="gadget.html" as="document"/>
+
+<link rel="prefetch" href="https://dhp.dreeshomes.com/cms/images/EYrud-a6KJ-Ayt63GXIVxF?scale=xxl" />
+
+<link rel="prefetch" href="https://homebase.rf.gd/homebaselogin/html/todo.html" />
+<a href="options.php"></a>
+
+<html style="width:700px;">
+<style>
+  .header {
+    background:black;
+    color:white;
+    width:100%;
+    padding: 5px 0;
+    font-size:30px;
+    text-align:center;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("https://dhp.dreeshomes.com/cms/images/EYrud-a6KJ-Ayt63GXIVxF?scale=xxl");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+font-family: 'Montserrat', sans-serif;
+
+  }
+  @keyframes a {
+      0% {
+          opacity:0;
+      }
+      99% {
+          opacity:0;
+      }
+      100% {
+          opacity:1
+      }
+  }
+  @keyframes c {
+      0% {
+          opacity:1;
+      }
+      99% {
+          opacity:1;
+      }
+      100% {
+          opacity:0
+      }
+  }
+  @keyframes b {
+      0% {
+          opacity:1;
+          border-radius:100%;
+          transform:scale(1);
+      }
+    
+      100% {
+          opacity:0;
+                    transform:scale(100);
+                              border-radius:100%;
+                              display:none;
+
+
+      }
+  }
+  .intro {
+        animation-name:b;
+animation-duration:.5s;
+animation-fill-mode:forwards;
+position:fixed;
+top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width:10px;
+  height:10px;
+  background:#eee;
+  z-index:99999;
+  z-index:-9999999;
+  animation-delay:1s;
+  }
+  .content {
+    margin:0;
+    animation-name:a;
+animation-duration:1s;
+animation-fill-mode:forwards;
+  }
+  .introa {
+    margin:0;
+    animation-name:c;
+animation-duration:1s;
+animation-fill-mode:forwards;
+z-index:99999999999999999;
+color:black;
+position:fixed;
+top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size:30px;
+  font-family:Arial
+  }
+  h1 {
+    margin:0;
+    padding: 0;
+  }
+  .header a {
+    color:white;
+  }
+  .container {
+    max-width:100%;
+    margin:auto;
+  }
+  .btn {
+    text-decoration: none;
+    padding:10px;
+    background:coral;
+    color:white;
+    margin:0;
+    font-size: 10px;
+  }
+  body {
+      margin:0;
+  }
+  .btn:hover {
+    text-decoration: none;
+    padding:10px;
+    background:rgb(214, 106, 66);
+    color:white;
+    z-index:999999999999999999999999999999999999;
+  }
+  .continually {
+position:fixed;
+font-family: 'Quicksand' !important;
+cursor:pointer;
+text-align:center;
+margin:10px;
+color:white !important;
+z-index:9999999999999999999999999999999999999999999999999999999999;
+padding:20px;
+border-radius:20px 0 20px 0px;
+}
+.continually:hover {
+background-color:#5d6d87;
+}
+</style>
+<div class="introa">Homebase</div>
+<div class="intro"></div>
+<div class="content">
+<div class="header"><br>
+<h1>Homebase</h1>
+<a href="http://homebase.rf.gd/homebase" class="btn" target="_blank">Launch Homebase</a>
+<a href="https://pramodini.github.io/Homebase/beta.htm" class="btn" target="_blank">Launch Homebase Free</a><br><br>
+
+</div><div class="container">
+  <h1 style="text-align: center;">My todo list</h1>
+                  <iframe src="https://homebase.rf.gd/homebaselogin/html/todo.html" width="100%" height="100%" frameborder="no" allowtransparency="allowtransparency"></iframe>
+
+</div>
+<a class="continually peach-gradient waves-effect"> <i class="fa fa-comment-alt"></i> <p>Need Help?</p></a>

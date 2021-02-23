0d3941944d270d3e7bf9cb8074075b117a9777ec
ManuTheCoder <77016441+ManuTheCoder@users.noreply.github.com>
2021-02-22 17:51:18 -0800

    Create options.js

---
+++ extension/options.js
@@ -0,0 +1,8 @@
+
+      var iFrameID = document.getElementById('idIframe');
+      if(iFrameID) {
+            // here you can make the height, I delete it first, then I make it again
+            iFrameID.height = "";
+            iFrameID.height = iFrameID.contentWindow.document.body.scrollHeight + "px";
+      }   
+iframeLoaded()

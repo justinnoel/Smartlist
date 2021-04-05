1d718205154e94f05cfba1346346d631a565549a
ManuTheCoder <hello@homebase.rf.gd>
2021-04-05 13:55:07 -0700

    Create custom_room_adder.php

---
+++ web/dashboard/rooms/custom_room_adder.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+if(!isset($_SESSION['valid'])) {
+    header('Location: login.php');
+}
+?>
+<?php
+$name = str_replace("<", "",str_replace("?", "", str_replace("'", "", $_POST['name'])));
+$qty = str_replace("<", "",str_replace("?", "", str_replace("'", "",$_POST['qty'])));
+$price = $_GET['room'];
+$loginId = $_SESSION['id'];
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql = "INSERT INTO custom_room_items(name, qty, price, login_id) 
+  VALUES('$name','$qty','$price', '$loginId')";
+  // use exec() because no results are returned
+  $conn->exec($sql);
+  //echo "New record created successfully";
+  header("Location: https://smartlist.ga/dashboard/test.php");
+} catch(PDOException $e) {
+  echo $sql . "<br>" . $e->getMessage();
+}
+
+$conn = null;
+?>

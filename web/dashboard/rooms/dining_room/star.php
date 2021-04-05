0a9ca42d6960dca0189556f3a932a010011761c1
ManuTheCoder <hello@homebase.rf.gd>
2021-04-05 14:25:11 -0700

    Create star.php

---
+++ web/dashboard/rooms/dining_room/star.php
@@ -0,0 +1,44 @@
+<?php 
+    session_start();
+    // if(!isset($_SESSION['valid'])) {header('Location: https://smartlist.ga/login');}
+    include('../../cred.php');
+    $id = $_GET['id'];
+    try {
+      $dbh = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+      $sql = "SELECT * FROM dining_room WHERE id=".$id;
+      $users = $dbh->query($sql);
+      foreach ($users as $row) {
+          $star  = $row['star'];
+      }
+      $dbh = null;
+    }
+    catch (PDOexception $e) {
+      echo "Error is: " . $e-> etmessage();
+    }
+    if($star == 0) {
+         try {
+          $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+          $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          $sql = "UPDATE dining_room SET star='1' WHERE id=$id";
+          $stmt = $conn->prepare($sql);
+          $stmt->execute();
+        } catch(PDOException $e) {
+          echo $sql . "<br>" . $e->getMessage();
+        }
+        
+        $conn = null;
+    }
+    elseif($star == 1) {
+         try {
+          $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+          $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          $sql = "UPDATE dining_room SET star='0' WHERE id=$id";
+          $stmt = $conn->prepare($sql);
+          $stmt->execute();
+        } catch(PDOException $e) {
+          echo $sql . "<br>" . $e->getMessage();
+        }
+        
+        $conn = null;
+    }
+?>

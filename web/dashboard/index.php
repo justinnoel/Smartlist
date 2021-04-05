64fe91775e6c159a490686c4e1b378e4a8eb5b17
ManuTheCoder <hello@homebase.rf.gd>
2021-04-05 14:29:47 -0700

    Create index.php

---
+++ web/dashboard/index.php
@@ -0,0 +1,10 @@
+<?php 
+if(isset($_SESSION['valid'])) { 
+    header('Location: https://smartlist.ga/dashboard/beta');
+    exit;
+}
+else {
+    header('Location: https://smartlist.ga/login');
+    exit;
+}
+?>

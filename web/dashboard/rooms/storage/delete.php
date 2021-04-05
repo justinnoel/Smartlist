9f91631e73f1568dc288e7c86299b1cbdacb42ef
ManuTheCoder <hello@homebase.rf.gd>
2021-04-05 14:12:49 -0700

    Create delete.php

---
+++ web/dashboard/rooms/storage/delete.php
@@ -0,0 +1,39 @@
+<?php session_start();
+$id = intval($_GET['id']);
+try {
+$dbh = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+$sql = "SELECT * FROM storageroom WHERE id=" . $_GET['id'];
+$users = $dbh->query($sql);
+foreach ($users as $row)
+{
+    $name = $row['name'];
+    $qty = $row['name'];
+}
+$dbh = null;
+}
+catch(PDOexception $e)
+{
+echo "Error is: " . $e->etmessage();
+}
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql = "DELETE FROM storageroom WHERE id=$id";
+  $conn->exec($sql);
+} catch(PDOException $e) {
+  echo $sql . "<br>" . $e->getMessage();
+}
+$conn = null;
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql = "INSERT INTO trash (name, qty, login, room)
+  VALUES (".json_encode($name).", ".json_encode($qty).", ".json_encode($_SESSION['id']).", 'Storage Room')";
+  $conn->exec($sql);
+} catch(PDOException $e) {
+  echo $sql . "<br>" . $e->getMessage();
+}
+
+$conn = null;
+?>
